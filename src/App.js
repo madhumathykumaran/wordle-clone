f14edcad874c165fb082481e97de98977e529aa3
Madhumathy <63185495+madhumathykumaran@users.noreply.github.com>
2023-03-14 18:50:08 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -66,21 +66,10 @@ function App() {
                     <li className="nav-item">
                       <a className="nav-link active" aria-current="page" href="#">Home</a>
                     </li>
-                    <li className="nav-item">
-                      <a className="nav-link" href="#">Archive</a>
-                    </li>
                   
                   </ul>
                   
-                </div>
-                {/* <div className="offcanvas-footer">
-                  <small className="copyright text-center">
-                    Made with ❤️ | Website developed by S Subodh
-                  </small><br></br>
-                  <small>
-                    This website is developed only for Educational/Learning purpose
-                  </small>
-                </div> */}
+              </div>
               </div>
             </div>
           </nav> 
@@ -180,22 +169,11 @@ function App() {
                     FAQ
                 </div>
               </div>
-              <div>
-                <small>
-                Made with ❤️ | Website developed by S Subodh
-                </small>
-              </div>
+              
               
 
             </div>
-            {/* <div className="modal-footer">
-              <small className="copyright text-center">
-                  Made with ❤️ | Website developed by S Subodh
-              </small>
-              <small>
-                This website is developed only for Educational purpose
-              </small>
-            </div> */}
+           
           </div>
         </div>
       </div>
@@ -359,11 +337,6 @@ function App() {
               
               <div className="mt-1">
                 NEXT WORDLE IN <span id='nextGameTimer'></span>
-              </div>
-              <div className="mt-1 stats-footer">
-                    <button className="button spellingBeeYellow" type="button">Play Spelling Bee <GiBee/></button>
-                    <button className="button share correct" type="button">Share <GiShare/></button>
-                  
               </div>
               
 
